39008eb6ce66f4f9e9723d7a34367abba0b182e9
ccustine <ccustine@apache.org>
2018-02-27 18:48:46 -0700

    kurasowa version v0.1.0

--- Formula/kurasowa.rb
+++ Formula/kurasowa.rb
@@ -3,7 +3,7 @@ class Kurasowa < Formula
   homepage "https://github.com/redhat-iot/kurasowa/"
   url "https://github.com/redhat-iot/kurasowa/releases/download/v0.1.0/kurasowa_0.1.0_macOS-64bit.tar.gz"
   version "0.1.0"
-  sha256 "be3460d1bf1ac30b0381ffd2d7be3e277793fd906d3e7e6d6e7002e1e577f8cf"
+  sha256 "2bd2b71650fbc6ccdb3c5b0b42a380bbc7c98175604023ee72ed45522016d0f8"
 
   def install
     bin.install "kurasowa"
